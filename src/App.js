7e5b8447890626593b2568fbde9c4fddcc87d036
John Canero <johncanero.official@gmail.com>
2022-07-27 13:42:48 +0800

    Specific Names using Props to Modify

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 
 // COMPONENT
-const Person = () => {
+const Person = (props) => {
   return (
     <>
-      <h1>Name: John</h1>
-      <h2>Last Name: Cañero</h2>
-      <h2>Age: 23</h2>
+      <h1>Name: {props.name}</h1>
+      <h2>Last Name: {props.lastName}</h2>
+      <h2>Age: {props.age}</h2>
     </>
   )
 }
@@ -18,7 +18,16 @@ const App = () => {
 
   return (
     <div className="App">
-      <Person />
+        <Person 
+        name={"Snowball"} 
+        lastName={"Jam"} 
+        age={12} 
+      />
+        <Person 
+        name={"Jolli"} 
+        lastName={"Bee"}
+        age={25} 
+       />
     </div>
   );
 }
