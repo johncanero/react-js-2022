3f6aa2bbd33d97f2d579e8189e8d9d5408e9fae6
John Canero <johncanero.official@gmail.com>
2022-07-27 19:53:48 +0800

    Input OMDb API (The Open Movie Database) and useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import { useEffect } from "react";
+
+// API KEY: 6c4bfdec
+
+// API KEY: OMDb API (The Open Movie Database) 
+const API_URL = 'http://www.omdbapi.com?apikey=6c4bfdec'
+
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman');
+    }, []);
+
     return (
         <h1>Hello Word of App</h1>
     );
