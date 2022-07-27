67b2c9c1a3abe49d815ab0b9b398c8acbf2d670b
John Canero <johncanero.official@gmail.com>
2022-07-27 15:47:30 +0800

    Delete src files, add new index.js and app.js, Publish Hello World of App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import React from 'react';
 
-
-// TRANSFER COMPONENT TO <[variable] />
 const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    alert("You've changed the counter to " + counter);
-  }, [counter]);
-
-  return (
-    <div className="App">
-        <button onClick={() => setCounter((prevCount) => prevCount -1 )}>-</button>
-        <h1>{counter}</h1>
-        <button onClick={() => setCounter((prevCount) => prevCount +1 )}>+</button>
-    </div>
-  );
+    return (
+        <h1>Hello Word of App</h1>
+    );
 }
 
 export default App;
