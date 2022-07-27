0c0676f7b1d61502c113265f5f913c615c1bb145
John Canero <johncanero.official@gmail.com>
2022-07-27 14:14:39 +0800

    useState and Button Click (Add and Subtract)

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
+import { useState } from 'react';
 import './App.css';
-
-// COMPONENT
-const Person = (props) => {
-  return (
-    <>
-      <h1>Name: {props.name}</h1>
-      <h2>Last Name: {props.lastName}</h2>
-      <h2>Age: {props.age}</h2>
-    </>
-  )
-}
 
 
 // TRANSFER COMPONENT TO <[variable] />
 const App = () => {
-  const name = 'Jam';
+  const [counter, setCounter] = useState(0);
 
   return (
     <div className="App">
-        <Person 
-        name={"Snowball"} 
-        lastName={"Jam"} 
-        age={12} 
-      />
-        <Person 
-        name={"Jolli"} 
-        lastName={"Bee"}
-        age={25} 
-       />
+        <button onClick={() => setCounter((prevCount) => prevCount -1 )}>-</button>
+        <h1>{counter}</h1>
+        <button onClick={() => setCounter((prevCount) => prevCount +1 )}>+</button>
     </div>
   );
 }
